6b21a0fecf045d24c392d481996eb6d0435599e7
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2023-07-07 15:47:17 -0500

    update home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import Todos from "../component/todos";
 import AddTodo from "../component/AddTodo";
 import "../pages/Home.css";
+import axios from 'axios';
 
 class Home extends Component {
-  // Create a default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
@@ -12,46 +12,37 @@ class Home extends Component {
     };
   }
 
-  // the deleteTodo function simply creates a new array that removes the todo item selected from the user from the list
-  // and then updates the state with the new list.
-  deleteTodo = (id) => {
-    // Within this function, the item's id is being utilized in order to filter it out from the todo list
-    // and then updates the state with a new list
-    const todos = this.state.todos.filter((todo) => {
-      return todo.id !== id;
-    });
-    this.setState({
-      todos: todos,
-    });
+  componentDidMount() {
+    this.fetchTodoItems();
+  }
+
+  fetchTodoItems = () => {
+    axios.get('http://localhost:3001/all/items')
+      .then(res => {
+        this.setState({ todos: res.data });
+      })
+      .catch(err => {
+        console.error(err);
+      });
   };
 
-  // the addTodo function simply creates a new array that includes the user submitted todo item and then
-  // updates the state with the new list.
-  addTodo = (todo) => {
-    const exists = this.state.todos.find(t => t.content === todo.content);
-    if (exists || todo.duedate == null || todo.duedate === "Invalid Date"){ return }
-    // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
-    // should not share duplicate values.
-    // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
-    // This solution works for a small application but a more complex hashing function should be used when
-    // dealing with a larger data sensitive project.
-    todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
-    this.setState({
-      todos: new_list,
-    });
+  deleteTodo = (id) => {
+    axios.delete(`http://localhost:3001/item/${id}`)
+      .then(res => {
+        if(res.status === 200) {
+          this.fetchTodoItems();
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
   };
+
   render() {
     return (
       <div className="Home">
         <h1>Todo's </h1>
-        {/* When passing the AddTodo component, addTodo is a prop that is used in the 
-        AddTodo.js file when handling the submit */}
-        <AddTodo addTodo={this.addTodo} />
-        {/* When returning the Todos component, todos is a prop passed to the todos.js file
-         to format and render the current todo list state */}
+        <AddTodo fetchTodoItems={this.fetchTodoItems} />
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
